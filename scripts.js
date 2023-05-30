cd4ca2da6c185cd30f556cf62fbbdb64f6b77f36
Maria Carolina <mariacarolina_bh@hotmail.com>
2023-05-30 18:31:00 -0300

    Configura página de pesquisa

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-/* Realiza o tratamento de paginação a partir da URL */
+/* Realiza o tratamento de paginação, termo e categoria a partir da URL */
 function obterPaginaAtual() {
   var urlAtual = new URL(window.location.href);
   var pagina = urlAtual.searchParams.get('pagina');
@@ -6,14 +6,41 @@ function obterPaginaAtual() {
   return pagina;
 }
 
+function obterTextoPesquisa() {
+  var urlAtual = new URL(window.location.href);
+  return urlAtual.searchParams.get('termo');
+}
+
+function obterCategoriaPesquisa() {
+  var urlAtual = new URL(window.location.href);
+  return urlAtual.searchParams.get('categoria');
+}
+
 /* Obtém os dados de produtos da API */
 function obterProdutos() {
-  fetch(`http://diwserver.vps.webdock.cloud:8765/products?page=${obterPaginaAtual()}&page_items=12`)
+  let url = `http://diwserver.vps.webdock.cloud:8765/products`;
+
+  var categoria = obterCategoriaPesquisa();
+  var termo = obterTextoPesquisa();
+  var pagina = obterPaginaAtual();
+
+  if (termo != null) {
+    url += `/search?query=${termo}`;
+    document.getElementById('descricao-pagina').innerHTML = `Buscando por: ${termo}`;
+  } else if (categoria != null) {
+    url += `/category/${categoria}?page=${pagina}&page_items=12`;
+    document.getElementById('descricao-pagina').innerHTML = `Buscando por: ${categoria}`;
+  } else {
+    url += `?page=${pagina}&page_items=12`;
+  }
+
+  fetch(url)
     .then(res => res.json())
     .then(data => {
       let str = ''
-      for (let i = 0; i < data.products.length; i++) {
-        let produto = data.products[i]
+      var produtos = data.products ? data.products : data;
+      for (let i = 0; i < produtos.length; i++) {
+        let produto = produtos[i];
         str += `<div class="col-md-4">
                 <div class="card" onclick="abrirDetalhes(${produto.id})">
                   <img src="${produto.image}" class="card-img-top">
@@ -23,9 +50,10 @@ function obterProdutos() {
                     <p class="card-text">R$ ${produto.price}</p>
                   </div>
                 </div>
-              </div>`
+              </div>`;
       }
-      document.getElementById('produtos-home').innerHTML = str
+      construirPaginacao(data.total_pages ?? 1);
+      document.getElementById('produtos-home').innerHTML = str;
     });
 }
 
@@ -37,7 +65,7 @@ function obterCategorias() {
       let str = ''
       for (let i = 0; i < data.length; i++) {
         let categoria = data[i].substring(data[i].indexOf("-") + 1).trim();
-        str += `<li><a href="pesquisa.html?categoria=${categoria}">${categoria}</a></li>`
+        str += `<li><a href="pesquisa.html?categoria=${data[i]}">${categoria}</a></li>`
       }
       document.getElementById('categorias').innerHTML = str
     })
@@ -50,27 +78,31 @@ function abrirDetalhes(id) {
 }
 
 /* Define automaticamente os valores da paginação */
-function construirPaginacao() {
+function construirPaginacao(limite) {
   var paginaAtual = obterPaginaAtual();
   document.getElementById('pagina-atual').innerHTML = paginaAtual;
   if (paginaAtual == 1) {
     document.getElementById('btn-anterior').classList.add('disabled');
   }
-  if (paginaAtual == 1214) {
+  if (paginaAtual == limite) {
     document.getElementById('btn-proximo').classList.add('disabled');
   }
 }
 
-/* Métodos para redirecionar para as páginas anterior e próxima */
-function paginaAnterior() {
-  var paginaAnterior = parseInt(obterPaginaAtual()) - 1;
-  var url = "index.html?pagina=" + encodeURIComponent(paginaAnterior);
-  window.location.href = url;
-}
-
-function proximaPagina() {
-  var proximaPagina = parseInt(obterPaginaAtual()) + 1;
-  var url = "index.html?pagina=" + encodeURIComponent(proximaPagina);
+/* Redirecionamento para as páginas anterior e próxima */
+function trocarPagina(valor) {
+  if ((valor == 1 && document.getElementById('btn-anterior').classList.contains('disabled')) || valor == 2 && document.getElementById('btn-proximo').classList.contains('disabled')) {
+    return;
+  }
+  var categoria = obterCategoriaPesquisa();
+  var pagina = parseInt(obterPaginaAtual()) + (valor == 1 ? -1 : 1);
+  let url = '';
+  if (categoria == null) {
+    url = `index.html?`;
+  } else {
+    url = `pesquisa.html?categoria=${categoria}&`
+  }
+  url += `pagina=${encodeURIComponent(pagina)}`;
   window.location.href = url;
 }
 
